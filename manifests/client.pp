a27d94e64a9da5e912f7c4101cbd87d23cbd7b93
Jakov Sosic <jsosic@gmail.com>
2016-10-04 12:45:15 +0200

    Use tools module for installing kernel packages (#1)

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,4 +1,5 @@
-# Class: fhgfs::client
+#
+# = Class: fhgfs::client
 #
 # This module manages FhGFS client
 #
@@ -7,30 +8,35 @@ class fhgfs::client (
   $mounts    = 'puppet:///private/fhgfs/fhgfs-mounts.conf',
   $netfilter = '',
 ) inherits fhgfs {
-  package { 'kernel-devel' :
-    ensure   => present,
-  }
+
+  include ::tools::kernel::devel
+
   package { 'fhgfs-helperd':
     ensure   => $version,
   }
+
   package { 'fhgfs-client':
     ensure   => $version,
     require  => Package['kernel-devel'],
   }
+
   service { 'fhgfs-helperd':
     ensure   => running,
     enable   => true,
     provider => redhat,
     require  => Package['fhgfs-helperd'],
   }
+
   file { '/etc/fhgfs/fhgfs-mounts.conf':
     require => Package['fhgfs-client'],
     source  => $mounts,
   }
+
   service { 'fhgfs-client':
     ensure   => running,
     enable   => true,
     provider => redhat,
     require  => [ Package['fhgfs-client'], Service['fhgfs-helperd'], File['/etc/fhgfs/fhgfs-mounts.conf'] ],
   }
+
 }
